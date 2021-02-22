8f2f3b7195ee75161bbe523461d545b465fe8da6
piyush1008 <51656856+piyush1008@users.noreply.github.com>
2021-02-22 13:51:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Navbar from './components/layout/Navbar';
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
+import NotFound from './components/pages/NotFound';
+import AddUsers from './components/users/AddUsers';
+import EditUsers from './components/users/EditUsers';
+import User from './components/users/User';
+
+
+function App() {
+  return (
+   <Router>
+      <>
+    <Navbar />
+    <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/contact' component={Contact} />
+        <Route exact path='/users/add' component={AddUsers} />
+        <Route exact path="/users/edit/:id" component={EditUsers} />
+          <Route exact path="/users/:id" component={User} />
+        <Route  component={NotFound} />
+    </Switch>
+    </>
+   </Router>
+  );
+}
+
+export default App;
